3aa8edbf366ca461eb9c77fcf2a1cc4ab096c560
DESKTOP-DJRPVMC\trido <torrydo.mobile@gmail.com>
2023-01-17 21:52:30 +0700

    update async test case

--- src/scraper/wiktionary/test_scrape_wiktionary.py
+++ src/scraper/wiktionary/test_scrape_wiktionary.py
@@ -1,14 +1,14 @@
-from unittest import TestCase
+from unittest import TestCase, IsolatedAsyncioTestCase
 
 from src.scraper.wiktionary.scrape_wiktionary import scrape_wiktionary_word
 
 
-class Test(TestCase):
-    def test_scrape_wiktionary_word(self):
+class Test(IsolatedAsyncioTestCase):
+    async def test_scrape_wiktionary_word(self):
         word = 'star'
-        vocab1 = scrape_wiktionary_word(word)
+        vocab1 = await scrape_wiktionary_word(word)
         self.assertTrue(vocab1 is not None)
 
         word2 = 'sdfadfs'
-        vocab2 = scrape_wiktionary_word(word2)
+        vocab2 = await scrape_wiktionary_word(word2)
         self.assertTrue(vocab2 is None)
